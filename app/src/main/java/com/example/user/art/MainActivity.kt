3a534305ac25c08430323a61974b97f4d5407c00
Tim Olson <mr.kaito01@gmail.com>
2018-10-27 17:43:49 +0900

    first

---
+++ app/src/main/java/com/example/user/art/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.user.art
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        reset.setOnClickListener {
+            val paintView = findViewById<View>(R.id.view) as PaintView
+            paintView.clear()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/user/art/PaintView.kt
@@ -0,0 +1,56 @@
+package com.example.user.art
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class PaintView (context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+
+    private val paint: Paint
+    private val path: Path
+
+    init {
+
+        path = Path()
+        paint = Paint()
+        paint.color = -0x1000000
+        paint.style = Paint.Style.STROKE
+        paint.strokeJoin = Paint.Join.ROUND
+        paint.strokeCap = Paint.Cap.ROUND
+        paint.strokeWidth = 10f
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawPath(path, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                path.moveTo(x, y)
+                invalidate()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                path.lineTo(x, y)
+                invalidate()
+            }
+            MotionEvent.ACTION_UP -> {
+                path.lineTo(x, y)
+                invalidate()
+            }
+        }
+        return true
+    }
+
+    fun clear() {
+        path.reset()
+        invalidate()
+    }
+}
